2bca2072e6e65fecb9a594e449e65ceef7f4d3a3
likviq <p3tro.pavlyk@gmail.com>
2023-01-14 18:00:51 +0200

    Repository pattern logic for user and queue entity

---
+++ SimpleQueue.Services/Repositories/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using SimpleQueue.Data;
+using SimpleQueue.Domain.Interfaces;
+using SimpleQueue.Domain.Models;
+
+namespace SimpleQueue.Services.Repositories
+{
+    public class UserRepository : RepositoryBase<User>, IUserRepository
+    {
+        public UserRepository(SimpleQueueDBContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+    }
+}
---
+++ SimpleQueue.Services/RepositoryManager.cs
@@ -0,0 +1,45 @@
+﻿using SimpleQueue.Data;
+using SimpleQueue.Domain.Interfaces;
+using SimpleQueue.Services.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleQueue.Services
+{
+    public class RepositoryManager : IRepositoryManager
+    {
+        private readonly SimpleQueueDBContext _context;
+        private IUserRepository _userRepository;
+        private IQueueRepository _queueRepository;
+
+        public RepositoryManager(SimpleQueueDBContext context)
+        {
+            _context = context;
+        }
+
+        public IUserRepository User
+        {
+            get
+            {
+                if (_userRepository == null)
+                    _userRepository = new UserRepository(_context);
+                return _userRepository;
+            }
+        }
+
+        public IQueueRepository Queue
+        {
+            get
+            {
+                if (_queueRepository == null)
+                    _queueRepository = new QueueRepository(_context);
+                return _queueRepository;
+            }
+        }
+
+        public void Save() => _context.SaveChanges();
+    }
+}
