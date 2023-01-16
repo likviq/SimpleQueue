801047d0a88be38047a197bc15c7a43c37f723e7
likviq <80317021+likviq@users.noreply.github.com>
2023-01-16 22:53:59 +0200

    add AutoMapper
    implement microsoft automapper and try it a bit with some dtos

---
+++ SimpleQueue/AutoMapper/MappingQueueProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SimpleQueue.WebUI.Models.DataTransferObjects;
+using SimpleQueue.Domain.Models;
+
+namespace SimpleQueue.WebUI.Automapper
+{
+    public class MappingQueueProfile : Profile
+    {
+        public MappingQueueProfile()
+        {
+            CreateMap<QueueForCreationDto, Queue>();
+        }
+    }
+}
