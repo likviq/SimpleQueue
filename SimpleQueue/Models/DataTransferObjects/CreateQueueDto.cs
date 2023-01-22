f4ef921d5d175cedfae34f5d491e1fffbcf20cd1
likviq <p3tro.pavlyk@gmail.com>
2023-01-22 18:22:49 +0200

    update createqueue dto

--- SimpleQueue/Models/DataTransferObjects/CreateQueueDto.cs
+++ SimpleQueue/Models/DataTransferObjects/CreateQueueDto.cs
@@ -6,5 +6,8 @@
         public string Description { get; set; }
         public float? Latitude { get; set; }
         public float? Longitude { get; set; }
+        public string? Password { get; set; }
+        public bool IsChat { get; set; }
+        public DateTime CreatedTime => DateTime.Now;
     }
 }
