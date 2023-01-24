c13f10b23842efea8c48d51a7856cac47029db31
likviq <p3tro.pavlyk@gmail.com>
2023-01-24 18:28:29 +0200

    added complex automapper(converted two models into one)

--- SimpleQueue/AutoMapper/MappingQueueProfile.cs
+++ SimpleQueue/AutoMapper/MappingQueueProfile.cs
@@ -13,7 +13,25 @@ namespace SimpleQueue.WebUI.Automapper
                 .ForMember(c => c.Chat,
                 opt => opt.MapFrom(x => x.IsChat));
 
-            CreateMap<Queue, GetQueueViewModel>();
+            CreateMap<UserInQueue, UserInQueueViewModel>()
+                .ForMember(c => c.Id,
+                opt => opt.MapFrom(x => x.User.Id))
+                .ForMember(c => c.Username,
+                opt => opt.MapFrom(x => x.User.Username))
+                .ForMember(c => c.Name,
+                opt => opt.MapFrom(x => x.User.Name))
+                .ForMember(c => c.Surname,
+                opt => opt.MapFrom(x => x.User.Surname))
+                .ForMember(c => c.JoinTime,
+                opt => opt.MapFrom(x => x.JoinTime))
+                .ForMember(c => c.IdInQueue,
+                opt => opt.MapFrom(x => x.Id))
+                .ForMember(c => c.NextIdInQueue,
+                opt => opt.MapFrom(x => x.NextId));
+
+            CreateMap<Queue, GetQueueViewModel>()
+                .ForMember(view => view.Users,
+                opt => opt.MapFrom(queue => queue.UserInQueues));
         }
     }
 }
--- SimpleQueue/Models/ViewModels/GetQueueViewModel.cs
+++ SimpleQueue/Models/ViewModels/GetQueueViewModel.cs
@@ -8,18 +8,7 @@
         public bool Chat { get; set; }
         public DateTime CreatedTime { get; set; }
         public DateTime StartTime { get; set; }
-        public IEnumerable<UserInQueueDto> Users { get; set; }
-        = Array.Empty<UserInQueueDto>();
-    }
-
-    public class UserInQueueDto
-    {
-        public Guid Id { get; set; }
-        public string Username { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public DateTime JoinTime { get; set; }
-        public Guid IdInQueue { get; set; }
-        public Guid? NextIdInQueue { get; set; }
+        public IEnumerable<UserInQueueViewModel> Users { get; set; }
+        = Array.Empty<UserInQueueViewModel>();
     }
 }
