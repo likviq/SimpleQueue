6c8acb2c679e6adca4fabf16c00a11f683d6796d
likviq <p3tro.pavlyk@gmail.com>
2023-01-26 14:49:41 +0200

    removed the temporary code

--- SimpleQueue.Data/Repositories/QueueRepository.cs
+++ SimpleQueue.Data/Repositories/QueueRepository.cs
@@ -5,23 +5,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace SimpleQueue.Data.Repositories
 {
-    public class UserDto
-    {
-        public Guid Id { get; set; }
-        public Guid IdInQueue { get; set; }
-        public string UserName { get; set; }
-        public DateTime JoinTime { get; set; }
-    }
-
-    public class QueueDto
-    {
-        public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public IEnumerable<UserDto> Users { get; set; }
-       = Array.Empty<UserDto>();
-    }
-
     public class QueueRepository : EFRepositoryBase<Queue>, IQueueRepository
     {
         public QueueRepository(SimpleQueueDBContext repositoryContext)
