6aac4a80aa047ccda749806dc1feb3ad45204536
Petro Pavliuk <80317021+likviq@users.noreply.github.com>
2023-01-26 21:23:11 +0200

    localization for layout page

---
+++ SimpleQueue/Controllers/CultureController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleQueue.WebUI.Controllers
+{
+    public class CultureController : Controller
+    {
+        [HttpPost]
+        public IActionResult SetCulture(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddDays(30) }
+            );
+            return LocalRedirect(returnUrl);
+        }
+    }
+}
--- SimpleQueue/Program.cs
+++ SimpleQueue/Program.cs
@@ -1,14 +1,18 @@
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using NLog.Web;
 using SimpleQueue.Data;
 using SimpleQueue.Domain.Interfaces;
 using SimpleQueue.Services;
 using SimpleQueue.WebUI.Automapper;
 using SimpleQueue.WebUI.Middlewares;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddViewLocalization();
 
 // NLog: Setup NLog for Dependency injection
 builder.Logging.ClearProviders();
@@ -24,6 +28,22 @@ builder.Services.AddDbContext<SimpleQueueDBContext>(options =>
 {
     var connectionString = builder.Configuration.GetConnectionString("mySqlConnection");
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+const string defaultCulture = "en";
+const string ukraineCultureName = "uk-UA";
+
+var supportedCultures = new[]
+{
+    new CultureInfo(defaultCulture),
+    new CultureInfo(ukraineCultureName)
+};
+builder.Services.Configure<RequestLocalizationOptions>(options => {
+    options.DefaultRequestCulture = new RequestCulture(defaultCulture);
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
 });
 
 var app = builder.Build();
@@ -43,6 +63,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseRequestLocalization(app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
 
 app.UseAuthorization();
 
