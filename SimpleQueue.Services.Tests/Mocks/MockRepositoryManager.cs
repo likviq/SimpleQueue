05b82c165cde82ca1a9a0832b4d8e01ecc6f7cca
likviq <p3tro.pavlyk@gmail.com>
2023-01-29 23:13:20 +0200

    add queue services(repository) unit tests

---
+++ SimpleQueue.Services.Tests/Mocks/MockRepositoryManager.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using SimpleQueue.Domain.Interfaces;
+
+namespace SimpleQueue.Services.Tests.Mocks
+{
+    internal class MockRepositoryManager
+    {
+        public static Mock<IRepositoryManager> GetMock()
+        {
+            var mock = new Mock<IRepositoryManager>();
+            var queueRepoMock = MockIQueueRepository.GetMock();
+
+            mock.Setup(m => m.Queue).Returns(() => queueRepoMock.Object);
+            mock.Setup(m => m.SaveAsync()).Callback(() => { return; });
+
+            return mock;
+        }
+    }
+    
+}
---
+++ SimpleQueue.Services.Tests/QueueServiceTests.cs
@@ -0,0 +1,56 @@
+﻿using AutoFixture;
+using Moq;
+using SimpleQueue.Domain.Entities;
+using SimpleQueue.Domain.Interfaces;
+using SimpleQueue.Services.Tests.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SimpleQueue.Services.Tests
+{
+    public class QueueServiceTests
+    {
+        private readonly Fixture _fixture;
+        private readonly Mock<IRepositoryManager> _mockRepositoryManager;
+        private readonly QueueService _mockQueueService;
+        public QueueServiceTests()
+        {
+            _fixture = new Fixture();
+            _mockRepositoryManager = MockRepositoryManager.GetMock();
+            _mockQueueService = new QueueService(_mockRepositoryManager.Object);
+        }
+        [Fact]
+        public async Task GivenAnIdOfAnExistingQueue_WhenGettingQueueById_ThenQueueReturns()
+        {
+            var id = Guid.Parse("0f8fad5b-d9cb-469f-a165-70867728950e");
+            
+            var result = await _mockQueueService.GetQueue(id);
+
+            Assert.NotNull(result);
+            Assert.IsAssignableFrom<Queue>(result);
+            Assert.NotNull(result as Queue);
+        }
+
+        [Fact]
+        public async Task GivenAnExistingQueue_WhenGettingQueueById_ThenQueueReturns()
+        {
+            var queue = new Queue()
+            {
+                Id = new Guid(),
+                Title = "Title",
+                Description = "Description",
+                Chat = true,
+                CreatedTime = DateTime.Now,
+                StartTime = DateTime.Now
+            };
+
+            var result = _mockQueueService.CreateQueue(queue);
+
+            Assert.NotNull(result);
+        }
+    }
+}
