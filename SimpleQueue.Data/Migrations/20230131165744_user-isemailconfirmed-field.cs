d0d14cb957cf14422fca3336f177afbe42e1ad19
likviq <p3tro.pavlyk@gmail.com>
2023-01-31 18:58:54 +0200

    updated user table

---
+++ SimpleQueue.Data/Migrations/20230131165744_user-isemailconfirmed-field.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleQueue.Data.Migrations
+{
+    public partial class userisemailconfirmedfield : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEmailConfirmed",
+                table: "Users",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsEmailConfirmed",
+                table: "Users");
+        }
+    }
+}
--- SimpleQueue.Domain/Entities/User.cs
+++ SimpleQueue.Domain/Entities/User.cs
@@ -9,6 +9,7 @@
         public string PhoneNumber { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
+        public bool IsEmailConfirmed { get; set; }
         public List<UserInQueue> UserInQueues { get; set; } = new List<UserInQueue>();
     }
 }
