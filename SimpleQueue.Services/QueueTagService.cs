d4d22e03e8de1eab0a46e16c7aa28ebece322470
likviq <p3tro.pavlyk@gmail.com>
2023-02-25 18:41:09 +0200

    tag repository, services for proper storage tag information in database)

---
+++ SimpleQueue.Services/QueueTagService.cs
@@ -0,0 +1,35 @@
+﻿using SimpleQueue.Domain.Entities;
+using SimpleQueue.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleQueue.Services
+{
+    public class QueueTagService : IQueueTagService
+    {
+        private readonly IRepositoryManager _repository;
+        public QueueTagService(IRepositoryManager repository)
+        {
+            _repository = repository;
+        }
+        public async Task<List<QueueTag>> InitializeTags(List<Tag> tagTitles)
+        {
+            var queueTags = new List<QueueTag>();
+            foreach (var tagTitle in tagTitles)
+            {
+                var tag = await _repository.Tag.GetTag(tagTitle.TagTitle);
+
+                var queueTagObject = new QueueTag(){
+                    TagId = tag.Id
+                };
+
+                queueTags.Add(queueTagObject);
+            }
+
+            return queueTags;
+        }
+    }
+}
---
+++ SimpleQueue.Services/TagService.cs
@@ -0,0 +1,41 @@
+﻿using SimpleQueue.Domain.Entities;
+using SimpleQueue.Domain.Interfaces;
+
+namespace SimpleQueue.Services
+{
+    public class TagService : ITagService
+    {
+        private readonly IRepositoryManager _repository;
+        public TagService(IRepositoryManager repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<Tag?> GetTag(string title)
+        {
+            var tag = _repository.Tag.GetTag(title);
+            return tag;
+        }
+
+        public Task<Tag?> GetTag(Guid id)
+        {
+            return _repository.Tag.GetTag(id);
+        }
+
+        public async Task CreateTags(List<Tag> tags)
+        {
+            var tagsList = new List<Tag>();
+
+            foreach(var tag in tags)
+            {
+                var tagDb = await GetTag(tag.TagTitle);
+                if (tagDb == null)
+                {
+                    tagsList.Add(tag);
+                }
+            }
+            _repository.Tag.CreateTags(tagsList);
+            _repository.Save();
+        }
+    }
+}
