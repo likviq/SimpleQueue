5d795181530c185e96939d64ca0a64df5c0d361c
likviq <p3tro.pavlyk@gmail.com>
2023-02-26 18:02:36 +0200

    add new fields to view(create queue)

--- SimpleQueue/wwwroot/js/create-queue.js
+++ SimpleQueue/wwwroot/js/create-queue.js
@@ -3,6 +3,15 @@ function changeQueuePrivacy() {
     var passwordInput = document.getElementById('password-value');
     passwordInput.disabled = !(checkbox === null || checkbox === void 0 ? void 0 : checkbox.checked);
 }
+function changeQueueType() {
+    var checkboxDelayedQueue = document.getElementsByClassName('delayed-queue')[0];
+    var ActiveTimeFromInput = document.getElementById('from-time');
+    var ActiveTimeToInput = document.getElementById('to-time');
+    var DurationTimeInput = document.getElementById('duration-time');
+    ActiveTimeFromInput.disabled = !(checkboxDelayedQueue === null || checkboxDelayedQueue === void 0 ? void 0 : checkboxDelayedQueue.checked);
+    ActiveTimeToInput.disabled = !(checkboxDelayedQueue === null || checkboxDelayedQueue === void 0 ? void 0 : checkboxDelayedQueue.checked);
+    DurationTimeInput.disabled = !(checkboxDelayedQueue === null || checkboxDelayedQueue === void 0 ? void 0 : checkboxDelayedQueue.checked);
+}
 function addTag() {
     var elem = document.querySelector('.tag-body:last-of-type');
     var clone = elem.cloneNode(true);
