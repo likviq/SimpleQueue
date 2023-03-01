f8555a57a150e1a0851e48be6fade0d8e45837ec
likviq <p3tro.pavlyk@gmail.com>
2023-03-01 15:27:17 +0200

    azure upload image

---
+++ SimpleQueue.Domain/Interfaces/IAzureStorage.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using SimpleQueue.Domain.Entities;
+
+namespace SimpleQueue.Domain.Interfaces
+{
+    public interface IAzureStorage
+    {
+        /// <summary>
+        /// This method uploads a file submitted with the request
+        /// </summary>
+        /// <param name="file">File for upload</param>
+        /// <returns>Blob with status</returns>
+        Task<ImageBlob> UploadAsync(IFormFile file);
+
+        /// <summary>
+        /// This method downloads a file with the specified filename
+        /// </summary>
+        /// <param name="blobFilename">Filename</param>
+        /// <returns>Blob</returns>
+        Task<ImageBlob> DownloadAsync(string blobFilename);
+
+        /// <summary>
+        /// This method deleted a file with the specified filename
+        /// </summary>
+        /// <param name="blobFilename">Filename</param>
+        /// <returns>Blob with status</returns>
+        Task<ImageBlob> DeleteAsync(string blobFilename);
+
+        /// <summary>
+        /// This method returns a list of all files located in the container
+        /// </summary>
+        /// <returns>Blobs in a list</returns>
+        Task<List<ImageBlob>> ListAsync();
+    }
+}
